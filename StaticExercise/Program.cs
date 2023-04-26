76dc0e3a7789158d0e19b6ac387b70a1dcf573c0
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-26 17:11:24 -0500

    Initial commit for Static Exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Fahrenheit -> Celcius");
+            var Fin = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(Fin));
+
+            Console.WriteLine("Celcius -> Fahrenheit");
+            var Cin = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(Cin));
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double deg)
+        {
+         
+            return (deg - 32)* 5/9;
+        }
+
+        public static double CelsiusToFahrenheit(double deg)
+        {
+           return (deg * 9 / 5) + 32; ;
+        }
+
+
+    }
+}
